dff3503fd5e8c07db21821ca570da089c9994d19
Ismael Kiprop <kipropismael27@gmail.com>
2023-08-08 14:52:22 -0700

    updated

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -2,7 +2,16 @@
 
 const closeButton = document.getElementById("navbar-close-btn");
 const menuToggle = document.querySelector(".navbar-toggler");
+const navLinks = document.querySelectorAll(".nav-link");
 
 menuToggle.addEventListener("click", () => {
     closeButton.classList.toggle("hide");
 });
+
+// Close menu when a navigation link is clicked
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        closeButton.classList.add("hide");
+    });
+});
+
