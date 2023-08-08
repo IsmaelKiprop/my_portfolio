045dcf5518f7cb23620ed940ad514e672a9c717e
Ismael Kiprop <kipropismael27@gmail.com>
2023-08-08 15:28:33 -0700

    UPDATED

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -1,17 +1,48 @@
 // js/menu-toggle.js
 
-const closeButton = document.getElementById("navbar-close-btn");
-const menuToggle = document.querySelector(".navbar-toggler");
-const navLinks = document.querySelectorAll(".nav-link");
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.querySelector(".navbar-toggler");
+    const closeMenuLinks = document.querySelectorAll(".close-menu");
+    const navLinks = document.querySelectorAll(".nav-link");
 
-menuToggle.addEventListener("click", () => {
-    closeButton.classList.toggle("hide");
-});
+    menuToggle.addEventListener("click", () => {
+        if (menuToggle.classList.contains("collapsed")) {
+            // Menu is open, close it
+            menuToggle.click();
+        }
+    });
 
-// Close menu when a navigation link is clicked
-navLinks.forEach(link => {
-    link.addEventListener("click", () => {
-        closeButton.classList.add("hide");
+    closeMenuLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            // Close the menu
+            menuToggle.click();
+        });
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            // Close the menu
+            menuToggle.click();
+        });
     });
 });
+
+// Get references to the menu toggle button and close button
+const menuToggleButton = document.querySelector('.navbar-toggler');
+const closeButton = document.querySelector('#navbar-close-btn');
+
+// Add event listener to the menu toggle button
+menuToggleButton.addEventListener('click', function () {
+    if (closeButton.style.display === 'none') {
+        closeButton.style.display = 'inline-block';
+    } else {
+        closeButton.style.display = 'none';
+    }
+});
+
+// Add event listener to the close button to hide it when clicked
+closeButton.addEventListener('click', function () {
+    closeButton.style.display = 'none';
+});
+
 
